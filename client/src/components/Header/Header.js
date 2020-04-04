7dde4504e0e2f48e7777a62052c9901aaa40d518
Laith Harb <harblaith77@gmail.com>
2020-04-03 21:58:25 -0400

    Added framer motion animation to header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import "./Header.scss"
 import React, { Component } from 'react'
+import {motion} from "framer-motion"
 
 export default class Header extends Component {
     render() {
@@ -7,10 +8,16 @@ export default class Header extends Component {
             <div className="Header">
                 <div className="Header__container">
                     <div className="Header__heading-container">
-                        <div className="Header__text-container">
+                        <motion.div 
+                            animate={{opacity: 1, x:100}} 
+                            initial={{opacity: 0}}
+                            transition={{duration:1.5}}
+                            className="Header__text-container"
+                        >
                             <h4 className="Header__hello-msg">Hello World</h4>
                             <h1 className="Header__name-msg">I'm Laith Harb</h1>
                             <h3 className="Header__position-msg">A Full-Stack Developer</h3>
+
                             <div className="Header__social-media-container">
                                 <div class="button-wrap facebook">
                                     <div class="button-inner-wrap">
@@ -18,26 +25,30 @@ export default class Header extends Component {
                                         <i class="icon-facebook active"></i>
                                     </div>
                                 </div>
-                                <div class="button-wrap facebook">
+                                <div class="button-wrap linkedin">
                                     <div class="button-inner-wrap">
                                         <i class="icon-linkedin inactive"></i>
                                         <i class="icon-linkedin active"></i>
                                     </div>
                                 </div>
-                                <div class="button-wrap facebook">
+                                <div class="button-wrap github">
                                     <div class="button-inner-wrap">
                                         <i class="icon-github inactive"></i>
                                         <i class="icon-github active"></i>
                                     </div>
                                 </div>
-                                <div class="button-wrap facebook">
+                                <div class="button-wrap google">
                                     <div class="button-inner-wrap">
                                         <i class="icon-google-plus inactive"></i>
                                         <i class="icon-google-plus active"></i>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+
+                            <button className="Header__project-btn">
+                                My Projects
+                            </button>
+                        </motion.div>
                     </div>
                     <div className="Header__image-container">
 
