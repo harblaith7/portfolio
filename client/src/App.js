8f3db8756a9d6a72d0cd151f58d0fd5157bb8cfd
Laith Harb <harblaith77@gmail.com>
2020-04-03 20:38:51 -0400

    Started header foundation

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import "./App.scss";
+import Header from "./components/Header/Header"
 
 
 export default class App extends Component {
   render() {
     return (
       <div>
-          hi
+          <Header/>
       </div>
     )
   }
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import "./Header.scss"
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="Header">
+                <div className="Header__container">
+                    <div className="Header__heading-container">
+                        <div className="Header__text-container">
+                            <h4 className="Header__hello-msg">Hello World</h4>
+                            <h1 className="Header__name-msg">I'm Laith Harb</h1>
+                            <h3 className="Header__position-msg">A Full-Stack Developer</h3>
+                            <div className="Header__social-media-container">
+                                <div class="button-wrap facebook">
+                                    <div class="button-inner-wrap">
+                                        <i class="icon-facebook inactive"></i>
+                                        <i class="icon-facebook active"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="Header__image-container">
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
