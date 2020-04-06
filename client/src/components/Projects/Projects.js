996c58ee9b2bb173e7d8211242d703054affe2c7
Laith Harb <harblaith77@gmail.com>
2020-04-05 21:38:23 -0400

    Fixed grammer errors in modal

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -24,20 +24,20 @@ export default class Projects extends Component {
                     name : "Blumber",
                     description: "Blumber is an online resources that helps university students ace their classes! It provides students with supplementary video lectures, online notes and practice problems.",
                     image: blumber,
-                    reason: "When I was in university, I found some courses to be extremely challenging. I would go to lectures, but there was no way I can grasp everything the professor was saying the first go round. Therefore, one day, I decided to video record the lecture and it helped immensely. That's where I got the idea for Blumber. I decided that it would be a great idea for previous student who have already excelled in the course to provided detailed video lectures about the university class. This way, a student can go over the concept at his/her pace and not have to worry about missing information.",
-                    techStack: "To build this application, I used React, Redux, Node, Express and MongoDB. Authentication was done via Passport.js and the user data was stored in a user document in MongoDB Atlas. Payment handling was done with Stripe.js. Redux was used to communicate authentication status to necessary pages and to centralize courses in the cart.",
+                    reason: "When I was in university, I found some courses to be extremely challenging. I would go to lectures, but there was no way I could grasp everything the professor was saying the first go round. Therefore, one day, I decided to video record the lecture and it helped immensely. That's who I got the idea for Blumber. I decided that it would be a great idea for previous student who have already excelled in the course to provided detailed video lectures about the university class. This way, a student can go over the concept at his/her pace and not have to worry about missing information.",
+                    techStack: "To build this application, I used React, Redux, Node, Express and MongoDB. Authentication was done via Passport.js and the user data was stored in a user document in MongoDB Atlas. Payment handling was done with Stripe.js. Redux was used to communicate authentication status to necessary pages and to centralize course state for the cart.",
                     techStackImages: [react, redux, mongo, passport, stripe, node],
-                    status: "Almost Complete / Not Deployed",
+                    status: "Almost Complete / Almost Responsive / Not Deployed",
                     url: "https://www.loom.com/share/c70d719d06684cabb217645cdaf173ab"
                 },
                 {
                     name: "Greatrr",
                     description: "Greatrr is a habit tracking and accountability applications. It allows you to reach your goals by tracking how you are spending your time and how often you are performing a habit.",
                     image: greatrr,
-                    reason: "In my forth year in university I read the book Outliers by Malcolm Gladwell. It was one of the most fascinating and informative book I've ever read. One concept that stood out to me was the fact that to be an outlier in a particular area you must first 10,000 hours to its mastery. It really got me thinking how much time was I devoting towards the things I wanted to be great at. To find out, I started tracking every minute of my day. I instantly become 10 times more productive and my self-esteem shot up. At the time I was tracking my habits and time in pen and paper. Now I want to make a real application that you and I can benefit from!",
-                    techStack: "To build this application, I used React, Redux, Node, Express and MongoDB. Authentication was done via Passport.js and the user data was stored in a user document in MongoDB Atlas. All non-user data are stored as subdocuments in the appropriates user document.",
+                    reason: "In my forth year in university I read the book Outliers by Malcolm Gladwell. It was one of the most fascinating and informative book I've ever read. One concept that stood out to me was the fact that to be an outlier in any particular field you must first devote 10,000 hours to its mastery. It really got me thinking how much time was I devoting towards the things I wanted to be great at. To find out, I started tracking every minute of my day. I instantly became 10 times more productive and my self-esteem shot up. At the time I was tracking my habits and how I was spending my time with pen and paper. Now I want to make a real application that you and I can benefit from!",
+                    techStack: "To build this application, I used React, Redux, Node, Express and MongoDB. Authentication was done via Passport.js and the user data was stored in a user document in MongoDB Atlas. All non-user data are stored as subdocuments in the appropriate user document.",
                     techStackImages: [react, redux, mongo, passport, node],
-                    status: "Current Project / Not Complete / Deployed",
+                    status: "Current Project / Deployed / Not Complete / Not Responsive",
                     url: "https://harb-greatrr.herokuapp.com/"
                 },
                 {
@@ -47,7 +47,7 @@ export default class Projects extends Component {
                     reason: "I want to show you my projects! I also wanted to use this as an opportunity to learn more about how to animate a react application.",
                     techStack: "I simply used React for this project. However, I am utilizing framer-motion to create smooth animations",
                     techStackImages: [react, framerMotion],
-                    status: "Almost Completed / Deployed",
+                    status: "Almost Complete / Deployed / Responsive",
                     url: "https://laithharb.com/"
                 },
                 {
@@ -57,7 +57,7 @@ export default class Projects extends Component {
                     reason: "This was done for a one day pair programming challenge. Building a regular snake game would have been too boring. We wanted to spice it up and have you control it with your own voice",
                     techStack: "We used React for this and JavaScript's built in voice recognition API",
                     techStackImages: [react],
-                    status: "Completed / Deployed",
+                    status: "Complete / Deployed / Not Responsive",
                     url: "https://stoic-hugle-8e3991.netlify.com/"
                 }
             ]
