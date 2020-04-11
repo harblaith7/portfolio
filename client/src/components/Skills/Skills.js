d2bfb0156603aa8b3727ee095bbe6895a0089bf2
Laith Harb <harblaith77@gmail.com>
2020-04-11 09:45:23 -0400

    Added skills section

--- client/src/components/Skills/Skills.js
+++ client/src/components/Skills/Skills.js
@@ -7,7 +7,12 @@ import node from "../../assets/Images/tech-stacks/nodejs.png";
 import mongo from "../../assets/Images/tech-stacks/mongo.png";
 import redux from "../../assets/Images/tech-stacks/redux.png";
 import mysql from "../../assets/Images/tech-stacks/mysql.png";
+import express from "../../assets/Images/tech-stacks/express.png";
+import graphql from "../../assets/Images/tech-stacks/graphql.png"
+import sass from "../../assets/Images/tech-stacks/sass.png";
 import typescript from "../../assets/Images/tech-stacks/Typescript.png";
+import questions from "../../assets/Images/question-mark.svg"
+import "csshake"
 
 export default class Skills extends Component {
 
@@ -16,6 +21,12 @@ export default class Skills extends Component {
         this.state = {
             skills : [
                 {
+                    image: questions,
+                    top: 52,
+                    right: 49,
+                    name: "question"
+                },
+                {
                     image : react,
                     top: 50,
                     right: 80,
@@ -23,34 +34,47 @@ export default class Skills extends Component {
                 },
                 {
                     image: node,
-                    top: 10,
-                    right: 60,
+                    top: 15,
+                    right: 70,
                     name: "node"
                 },
                 {
                     image: redux,
-                    top: 50,
-                    right: 35,
+                    top: 100,
+                    right: 47.5,
                     name: "redux"
                 },
                 {
                     image: mongo,
-                    top: 90,
-                    right: 60,
+                    top: 85,
+                    right: 70,
                     name: "mongo"
                 },
                 {
                     image: typescript,
-                    top: 90,
-                    right: 15,
+                    top: 85,
+                    right: 25,
                     name: "typescript"
                 },
                 {
                     image: mysql,
-                    top: 10,
-                    right: 15,
+                    top: 15,
+                    right: 25,
                     name: "mysql"
-                }
+                },
+                {
+                    image: graphql,
+                    top: 50,
+                    right: 15.5,
+                    name: "graphql"
+                },
+                {
+                    image: express,
+                    top: 0,
+                    right: 47.5,
+                    name: "express"
+                },
+                
             ]
         }
     }
@@ -60,7 +84,7 @@ export default class Skills extends Component {
         return this.state.skills.map(skill => {
             return (
                 <motion.div 
-                    className="Skills__node"
+                    className={`Skills__node shake-little ${skill.name === "question" && "Skills__node--small"}`}
                     id={skill.name}
                     style={{
                         backgroundImage : `url(${skill.image})`,
@@ -90,19 +114,15 @@ export default class Skills extends Component {
                     <h3 className="Skills__heading">
                         My Skills
                     </h3>
-                    <div className="Skills__graph-container">
-                       {this.displayNodes()}
-
-                       <svg id="svg1" width="100" height="0" >
-                            <path
-                                id="myNewPath"
-                                d="M0 0"             
-                                stroke-width="0.3em"
-                                style={{stroke:"#555", fill:"none"}}
-                            />
-                       </svg>
-
+                    <div className="Skills__skills-container">
+                        <div className="Skills__graph-container">
+                            {this.displayNodes()}
+                        </div>
+                        <div className="Skills__description">
+                            adasd
+                        </div>
                     </div>
+                    
                 </div>
             </motion.div>
         )
